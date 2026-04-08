3c5a536af88fc0ee9c3a26196b064c32420d37b4
Larry Wiggins <wiggins3dev@gmail.com>
2026-04-08 16:37:15 -0500

    Fixing folder structure

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CampoutPermissionForm from "./CampoutPermissionForm";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CampoutPermissionForm />
+  </React.StrictMode>
+);
